30c0c0a34622c135aedc90aa1349a312aeb1f2bd
Barma Vishwanth <vishwanthbsavp@gmail.com>
2024-07-26 19:02:21 +0530

    fix: resume-3d-card

--- src/components/global/text-generate-effect.tsx
+++ src/components/global/text-generate-effect.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { useEffect } from "react";
+
+import { useEffect, useState } from "react";
 import { motion, stagger, useAnimate } from "framer-motion";
 import { cn } from "@/lib/utils";
 
@@ -8,31 +9,46 @@ export const TextGenerateEffect = ({
   className,
   filter = true,
   duration = 0.5,
+  loopDuration = 6000, // Duration of the entire loop (in milliseconds)
 }: {
   words: string;
   className?: string;
   filter?: boolean;
   duration?: number;
+  loopDuration?: number; // Duration of one complete loop
 }) => {
   const [scope, animate] = useAnimate();
   let wordsArray = words.split(" ");
+  const [animationKey, setAnimationKey] = useState(Date.now()); // Key to trigger re-render
+
   useEffect(() => {
-    animate(
-      "span",
-      {
-        opacity: 1,
-        filter: filter ? "blur(0px)" : "none",
-      },
-      {
-        duration: duration ? duration : 1,
-        delay: stagger(0.2),
-      }
-    );
-  }, [scope.current]);
+    // Function to handle animation
+    const animateText = () => {
+      animate(
+        "span",
+        {
+          opacity: 1,
+          filter: filter ? "blur(0px)" : "none",
+        },
+        {
+          duration: duration ? duration : 1,
+          delay: stagger(0.2),
+          onComplete: () => {
+            // Restart animation after loopDuration
+            setTimeout(() => {
+              setAnimationKey(Date.now()); // Change key to force re-render
+            }, loopDuration);
+          }
+        }
+      );
+    };
+
+    animateText();
+  }, [animationKey]); // Depend on animationKey to restart the animation
 
   const renderWords = () => {
     return (
-      <motion.div ref={scope}>
+      <motion.div ref={scope} key={animationKey}>
         {wordsArray.map((word, idx) => {
           return (
             <motion.span
@@ -53,7 +69,7 @@ export const TextGenerateEffect = ({
   return (
     <div className={cn("font-bold", className)}>
       <div className="">
-        <div className=" dark:text-white text-black text-[6px] leading-snug tracking-wide">
+        <div className="dark:text-white text-black text-[6px] leading-snug tracking-wide">
           {renderWords()}
         </div>
       </div>
