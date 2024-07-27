2691ed4d566b1287b3dc3e29ef080845410c1f20
Barma Vishwanth <vishwanthbsavp@gmail.com>
2024-07-27 17:20:49 +0530

    changing uploadfile

--- src/components/enhance-resume/upload-file.tsx
+++ src/components/enhance-resume/upload-file.tsx
@@ -1,71 +1,14 @@
-import { CloudUpload, Inbox } from 'lucide-react'
-import React, { useState } from 'react'
-import { useDropzone } from 'react-dropzone'
+import React from 'react'
 
 interface Props {
     
 }
 
 const UploadFile = (props: Props) => {
-
-    const [loading, setLoading] = useState(false);
-
-    const {getRootProps, getInputProps} = useDropzone({
-        accept: {"application/pdf" : [".pdf"]},
-        maxFiles: 1,
-        onDrop: async (acceptedFiles) => {
-            const file = acceptedFiles[0];
-            if (file.size > 10 * 1024 * 1024) {
-              console.log("Pdf size is >10mb")
-              // show it on display
-              return;
-            }
-
-            parseResume(file);
-
-        }
-    })
-
-    const parseResume = async (file: File) => {
-        console.log(file);
-
-        const formData = new FormData();
-        formData.append('file', file);
-
-        console.log(formData);
-
-        try{
-            const response = await fetch('/api/enhance-resume/upload-resume', {
-                method: 'POST',
-                body: formData,
-            })
-
-            const result = await response.json();
-            console.log("Result From Server: ", result);
-
-        }catch(error){
-            console.error('Error uploading file: ', error);
-        }
-    }
-
-
-
-
     return (
-        <div className="p-2 rounded-xl">
-        <div
-          {...getRootProps({
-            className:
-              "border-dashed border-2 rounded-xl cursor-pointer w-[20rem] py-10 flex justify-center items-center flex-col",
-          })}
-        >
-          <input {...getInputProps()} />
-            <>
-              <CloudUpload className="w-14 h-14" />
-              <p className="mt-2 text-sm text-slate-200">Drop Your Resume Here</p>
-            </>
+        <div>
+            <h1>hello world</h1>
         </div>
-      </div>
     )
 }
 
