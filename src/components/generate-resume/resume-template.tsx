775e5d1204e1e33b1e8d6452daa3c2cece3da8f1
Barma Vishwanth <vishwanthbsavp@gmail.com>
2024-08-08 19:03:14 +0530

    fix: Image/nextjs component (width $ height)

--- src/components/generate-resume/resume-template.tsx
+++ src/components/generate-resume/resume-template.tsx
@@ -24,7 +24,7 @@ const ResumeTemplate = ({title, img, url, disable}: Props) => {
   return (
     <div className={`m-2 hover:scale-105 transition ease-in-out mb-5 ${disable && 'cursor-not-allowed opacity-50'}`}>
         <button onClick={handleClick} className={disable && 'cursor-not-allowed'}>
-            <Image alt='Resume Template Name' className='h-[500px] rounded-xl' src={img}></Image>
+            <Image alt='Resume Template Name' width={300} height={600} className='rounded-xl' src={img}></Image>
             <p className='text-center font-bold mt-2'>{title}</p>
         </button>
     </div>
